d3ed63ad15e870b44eeb618b032c236a13aa7b31
gustavocarrillo <gustavoinolympus@gmail.com>
2018-02-20 14:54:38 -0400

    Instalacion de plantilla.

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,43 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('/plantilla', function () {
+    return view('plantilla');
+})->name('pl');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+
+Route::middleware(['auth'])->group(function (){
+
+    Route::get('/admin', function () {
+        return view('admin.admin');
+    });
+
+    //Rutas para registro de usuarios
+    Route::get('/registro', function () {
+        return view('auth.registro');
+    });
+
+    Route::post('/registrar','Auth\RegistroController@nuevo')->name('registrar');
+
+    //Rutas para incorporacion de bienes
+    Route::get('/incorporacion',function (){
+        return view("gestion_bienes.nuevo");
+    })->name('incorporacion');
+
+    Route::post('/incorporar')->name('incorporar');
+
+    //Ruta para salir del sistema.
+    Route::get('/salir',function (){
+        Auth::logout();
+        return redirect('/login');
+    })->name('salir');
+});
+
+Route::post('/autenticar','Auth\LoginController@autenticar')->name('loguear');
