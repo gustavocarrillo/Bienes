65f6f6dfbf4ad30931c61f259d6e9336bcbbb38a
Gustavo Carrillo <gcarrillo.wx@gmail.com>
2018-12-13 16:56:40 -0400

    BM1

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Bien;
+use App\Elemento;
 use Illuminate\Http\Request;
 use App\Departamento;
 use App\Direccion;
@@ -12,6 +14,34 @@ class ReportesController extends Controller
     public function BM1($tipoUnidad ,$id)
     {
         $data = '';
+        $count = [];
+        $elementos = Elemento::all();
+
+        foreach ($elementos as $elemento) {
+            if($tipoUnidad == "direccion") {
+                $data = Bien::where('direccion', $id)
+                    ->where('codigo', 'like', $elemento->codigo . '%');
+            }elseif ($tipoUnidad == "departamento"){
+                $data = Bien::where('departamento', $id)
+                    ->where('codigo', 'like', $elemento->codigo . '%');
+            }
+
+            if (count($data->get()) > 0) {
+                $bien = $data->first();
+                $bien->count = $data->count();
+                array_push($count, $bien);
+            }
+        }
+        $data = $count;
+        $pdf = PDF::loadView('PDF.bm1', compact('data'));
+        $pdf->setPaper('A4','landscape');
+//        return view('PDF.bm1')->with(compact('data','bienes_dep'));
+        return $pdf->download("bm1.pdf");
+    }
+
+    public function BM2($tipoUnidad ,$id)
+    {
+        $data = '';
         $bienes_dep = Departamento::where('codigo','10-06')->first();
 
         if($tipoUnidad == "direccion"){
@@ -19,10 +49,10 @@ class ReportesController extends Controller
         }elseif ($tipoUnidad == "departamento"){
             $data = Departamento::with('bienes','_direccion')->where('id',$id)->first();
         }
-        //dd($data->toArray());
-        $pdf = PDF::loadView('PDF.bm1', compact('data','bienes_dep'));
-        //$pdf->setPaper('A4','landscape');
-        //return view('PDF.bm1')->with(compact('data','bienes_dep'));
+//        dd($data->toArray());
+        $pdf = PDF::loadView('PDF.bm1', compact('data'));
+        $pdf->setPaper('A4','landscape');
+//        return view('PDF.bm1')->with(compact('data','bienes_dep'));
         return $pdf->download("bm1.pdf");
     }
 }
--- resources/views/PDF/bm1.blade.php
+++ resources/views/PDF/bm1.blade.php
@@ -9,89 +9,94 @@
     <!-- Bootstrap Core Css -->
     <link href="{{ asset("plugins/bootstrap/css/bootstrap.css") }}" rel="stylesheet">
     <link href="{{ asset("css/style.css") }}" rel="stylesheet">
-
+    <style>
+        .saltopagina{page-break-after:always;}
+    </style>
 </head>
 <body style="background-color: white">
-    <div class="align-center">
-        <h4>Departamento de Bienes Municipales</h4>
-        <h4>Inventarion de Bienes Muebles</h4>
-    </div>
+<div class="align-center">
+    <h4>Departamento de Bienes Municipales</h4>
+    <h4>Inventarion de Bienes Muebles</h4>
+</div>
+<br>
+<br>
+<div class="col-lg-6 col-md-6">
+    <{{--div>
+        <p>Entidad propietaria: <strong>ALCALDIA DE MATURIN</strong></p>
+        <p>Servicio:<strong> @if($data->_direccion) {{ $data->_direccion->descripcion }} @else {{ $data->descripcion }} @endif</strong></p>
+        <p>Unida de trabajo o dependencia:<strong> @if($data->_direccion) {{ $data->_direccion->descripcion }} @else {{ $data->descripcion }} @endif</strong></p>
+        <p>Dirección o lugar: <strong>Calle Azcúe, Edificio Palacio Municipal, Maturín Estado Monagas. </strong></p>
+    </div>--}}
     <br>
-    <br>
-    <div class="col-lg-6 col-md-6">
-        <div>
-            <p><strong>Entidad propietaria: ALCALDIA DE MATURIN</strong></p>
-            <p><strong>Servicio: @if($data->_direccion) {{ $data->_direccion->descripcion }} @else {{ $data->descripcion }} @endif</strong></p>
-            <p><strong>Unida de trabajo o dependencia: @if($data->_direccion) {{ $data->descripcion }} @else {{ $data->_direccion->descripcion }} @endif</strong></p>
-            <p><strong>Dirección o lugar: Calle Azcúe, Edificio Palacio Municipal, Maturín Estado Monagas. </strong></p>
-        </div>
-        <br>
+    <div class="">
         <div class="">
-            <div class="">
-                <div class="table-responsive">
-                    <table class="table table-bordered">
-                        <thead>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>N°</th>
+                        <th>Fecha</th>
+                        <th>Codigo</th>
+                        <th>Descripcion</th>
+                        <th>Cantidad</th>
+                        <th width="15%">Valor Unit.</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {{ $total = 0 }}
+                    {{ $n = 1 }}
+                    @foreach($data as $bien)
                         <tr>
-                            <th>N°</th>
-                            <th>Fecha</th>
-                            <th>Codigo</th>
-                            <th>Descripcion</th>
-                            <th width="15%">Valor Unit.</th>
+                            <td width="3%">{{ $n }}</td>
+                            <td width="5%">{{ date('d-m-Y',strtotime($bien->fecha_incorp)) }}</td>
+                            <td width="10%">{{ $bien->codigo }}</td>
+                            <td width="30%">{{ $bien->descripcion }}</td>
+                            <td width="30%">{{ $bien->count }}</td>
+                            <td width="5%">{{ $bien->valor_actual }}</td>
                         </tr>
-                        </thead>
-                        <tbody>
-                        {{ $total = 0 }}
-                        {{ $n = 1 }}
-                        @foreach($data->bienes as $bien)
-                            <tr>
-                                <td width="3%">{{ $n }}</td>
-                                <td width="5%">{{ date('d-m-Y',strtotime($bien->fecha_incorp)) }}</td>
-                                <td width="10%">{{ $bien->codigo }}</td>
-                                <td width="30%">{{ $bien->descripcion }}</td>
-                                <td width="5%">{{ $bien->valor_actual }}</td>
-                            </tr>
-                            {{ $n++ }}
-                            {{ $total+= $bien->valor_actual}}
-                        @endforeach
-                            <tr style="background-color: #b8e834">
-                                <td colspan="4"><strong>TOTAL:</strong></td>
-                                <td><strong>{{ $total }}</strong></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                        {{ $n++ }}
+                        {{ $total+= $bien->valor_actual}}
+                    @endforeach
+                    <tr style="background-color: #b8e834">
+                        <td colspan="5"><strong>TOTAL:</strong></td>
+                        <td><strong>{{ $total }}</strong></td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
-        <br>
-        <br>
-        <br>
-        <table>
-            <tr>
-                <td width="340px" class="align-center">DEPARTAMENTO DE BIENES</td>
-                <td width="340px" class="align-center">@if($data->_direccion) {{ $data->_direccion->descripcion }} @else {{ $data->descripcion }} @endif</td>
-            </tr>
-            <tr>
-                <td><span style="color: white">texto</span></td>
-                <td><span style="color: white">texto</span></td>
-            </tr>
-            <tr>
-                <td><span style="color: white">texto</span></td>
-                <td><span style="color: white">texto</span></td>
-            </tr>
-            <tr>
-                <td width="340px" class="align-center">_______________________</td>
-                <td width="340px" class="align-center">_______________________</td>
-            </tr>
-            <tr>
-                <td width="340px" class="align-center">{{ strtoupper($bienes_dep->responsable) }}</td>
-                <td width="340px" class="align-center">@if($data->_direccion) {{ $data->_direccion->responsable }} @else {{ $data->responsable }} @endif</td>
-            </tr>
-            <tr>
-                <td width="340px" class="align-center">{{ strtoupper($bienes_dep->resolucion) }}</td>
-                <td width="340px" class="align-center">@if($data->_direccion) {{ $data->_direccion->resolucion }} @else {{ $data->resolucion }} @endif</td>
-            </tr>
-        </table>
     </div>
+    <br>
+    <br>
+    <br>
+</div>
+<div class="saltopagina"></div>
+<table class="table-responsive" style="border: 0px">
+    {{--<tr>
+        <td class="align-center">DEPARTAMENTO DE BIENES</td>
+        <td class="align-center">@if($data->_direccion) {{ $data->_direccion->descripcion }} @else {{ $data->descripcion }} @endif</td>
+    </tr>--}}
+    <tr>
+        <td><span style="color: white">texto</span></td>
+        <td><span style="color: white">texto</span></td>
+    </tr>
+    <tr>
+        <td><span style="color: white">texto</span></td>
+        <td><span style="color: white">texto</span></td>
+    </tr>
+    <tr>
+        <td class="align-center">_______________________</td>
+        <td class="align-center">_______________________</td>
+    </tr>
+    {{-- <tr>
+         <td width="340px" class="align-center">{{ strtoupper($bienes_dep->responsable) }}</td>
+         <td width="340px" class="align-center">@if($data->_direccion) {{ $data->_direccion->responsable }} @else {{ $data->responsable }} @endif</td>
+     </tr>
+     <tr>
+         <td width="340px" class="align-center">{{ strtoupper($bienes_dep->resolucion) }}</td>
+         <td width="340px" class="align-center">@if($data->_direccion) {{ $data->_direccion->resolucion }} @else {{ $data->resolucion }} @endif</td>
+     </tr>--}}
+</table>
 </body>
 </html>
 
